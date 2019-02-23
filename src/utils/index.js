1161117c2ad05f35bfae577219ac88a1a6e1fd6d
Mykola Savenko <biosis21@gmail.com>
2019-02-23 22:50:59 +0100

    Add comments to the utils files

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,19 @@
+/**
+ * Function for converting array to object with specific prop
+ * @example
+ *
+ * from
+ * const arr = [{id: 1, name: "Tom"}, {id: 2, name: "Jack"}]
+ *
+ * fromArrayToObject(arr, "id");
+ *
+ * to
+ * {1: {id: 1, name: "Tom"}, 2: {id: 2, name: "Jack"}}
+ *
+ * @param {array} arr
+ * @param {string} prop
+ * @returns {array}
+ */
 export const fromArrayToObject = (arr = [], prop) => {
     return arr.reduce((acc, v, i) => {
         const c = v[prop] || i;
